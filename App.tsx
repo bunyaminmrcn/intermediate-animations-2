2cc32c7bda78d140a3ca45f8574b676bf72ce490
bunyaminmrcn <bunyaminmrcn@gmail.com>
2023-10-15 18:55:18 +0300

    Lesson #13

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import Lesson_11 from "./src/lesson_11";
 import Lesson_12 from "./src/lesson_12";
+import Lesson_13 from "./src/lesson_13";
 
 export default function App() {
-  return <Lesson_12 />
+  return <Lesson_13 />
 }
